32a01f09182f9f3220164cb386593c1186838179
Jack0Chan <48609550+Jack0Chan@users.noreply.github.com>
2022-08-23 10:19:48 +0800

    消除架构修改后的部分bug

--- src/pyuppaal/__init__.py
+++ src/pyuppaal/__init__.py
@@ -1,10 +1,8 @@
 from .verifyta import Verifyta
-from .umodel import UModel
+from .umodel import UModel, TimedActions
 from .tracer import ClockZone, Transition, SimTrace, Tracer
 from .config import *
 from .pyuppaal import *
-from .namedtuple import *
-
 
 
 # __all__ = [ROOT_DIR, TRACER_CUSTOM_WINDOWS,TRACER_CUSTOM_LINUX]
