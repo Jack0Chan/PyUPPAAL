8b236189307702cd6c2d9d3c56779cadabcc1f2e
Ducky-DuckyD <dyy2221503927@163.com>
2022-08-31 15:44:03 +0800

    0.1.0

--- src/pyuppaal/__init__.py
+++ src/pyuppaal/__init__.py
@@ -1,5 +1,5 @@
 from .verifyta import Verifyta
-from .umodel import UModel, TimedActions
+from .umodel import UModel
 from .tracer import ClockZone, Transition, SimTrace, Tracer
 from .config import *
 from .pyuppaal import *
