889a0ade9e005003f05c83a3dd5172c12b0ffc5b
Ducky-DuckyD <dyy2221503927@163.com>
2022-08-31 18:53:23 +0800

    0.1.1

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 import setuptools
+import re
+
+def get_property(prop, project):
+    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop), open('./src/pyuppaal/__init__.py').read())
+    return result.group(1)
+
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="pyuppaal",
-    version="0.1.0",
+    version = get_property('__version__', 'pyuppaal'),
     author="Jack0Chan",
     author_email="",
     description="First public beta version: 0.1.0. A research tool that can simulate, verify or modify UPPAAL models with python. It can also help to analyze counter-examples in .xml format",
