36f7484832041c3997bcda549cfcd313c3aef553
Ducky-DuckyD <dyy2221503927@163.com>
2022-09-03 15:43:23 +0800

    0.1.4 extension

--- src/tests/test_umodel.py
+++ src/tests/test_umodel.py
@@ -3,6 +3,6 @@
 import pyuppaal as pyu
 # import iTools/buildCG as cg // fault
 
-pyu.get_communication_graph(r'C:\Users\22215\OneDrive\Coding\PlayGround\Github\pyuppaal\src\tests\Pedestrian.xml',r'C:\Users\22215\OneDrive\Coding\PlayGround\Github\pyuppaal\src\tests\Pedestrian.png')
+pyu.get_communication_graph(r'C:\Users\22215\OneDrive\Coding\Github\pyuppaal\src\tests\Pedestrian.xml',r'C:\Users\22215\OneDrive\Coding\Github\pyuppaal\src\tests\Pedestrian.png')
 
-pyu.get_communication_graph(r'C:\Users\22215\OneDrive\Coding\Lab\AVRT_Complete_GroundTruth.xml',r'C:\Users\22215\OneDrive\Coding\Lab\AVRT_Complete_GroundTruth.png')
+# pyu.get_communication_graph(r'C:\Users\22215\OneDrive\Coding\Lab\AVRT_Complete_GroundTruth.xml',r'C:\Users\22215\OneDrive\Coding\Lab\AVRT_Complete_GroundTruth.png')
