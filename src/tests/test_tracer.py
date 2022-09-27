490c7d67b4ef507a597b61b541f86e3c63b99989
Jack0Chan <48609550+Jack0Chan@users.noreply.github.com>
2022-09-27 16:04:52 +0800

    更新Tracer的架构

--- src/tests/test_tracer.py
+++ src/tests/test_tracer.py
@@ -1,11 +1,13 @@
 """_summary_
 """
+import os
 from pyuppaal import Tracer
 from pyuppaal import Verifyta
-from verifyta_path import VERIFYTA_PATH
+from verifyta_path import ROOT_DIR,VERIFYTA_PATH
 
 Verifyta().set_verifyta_path(VERIFYTA_PATH)
-model_path = '/Users/chenguangyao/Documents/GitHub/pyuppaal/src/tests/pedestrian.xml'
-trace_path = '/Users/chenguangyao/Documents/GitHub/pyuppaal/src/tests/pedestrian-1.xtr'
+model_path = os.path.join(ROOT_DIR, 'pedestrian.xml')
+# model_path = os.path.join(ROOT_DIR, 'AVNRT_Fake_GroundTruth.xml')
+trace_path = os.path.join(ROOT_DIR, 'pedestrian-1.xtr')
 Verifyta().easy_verify(model_path)
-Tracer.get_timed_trace(model_path, trace_path)
+print(Tracer.get_timed_trace(model_path, trace_path, hold=False))
