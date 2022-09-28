0a45076bd577a850e42171c87da27702230aa931
Jack0Chan <48609550+Jack0Chan@users.noreply.github.com>
2022-09-28 10:03:11 +0800

    macos测试失败

--- src/tests/test_tracer.py
+++ src/tests/test_tracer.py
@@ -1,17 +1,16 @@
 """_summary_
 """
-import os
+import verifyta_path
 from pyuppaal import Tracer
 from pyuppaal import Verifyta
-from verifyta_path import *
 
-Verifyta().set_verifyta_path(VERIFYTA_PATH)
-model_path = bring_to_root('AVNRT_Fake_GroundTruth.xml')
-trace_path = bring_to_root('AVNRT_Fake_GroundTruth-1.xtr')
+Verifyta().set_verifyta_path(verifyta_path.VERIFYTA_PATH)
+model_path = verifyta_path.bring_to_root('AVNRT_Fake_GroundTruth.xml')
+trace_path = verifyta_path.bring_to_root('AVNRT_Fake_GroundTruth-1.xtr')
 Verifyta().easy_verify(model_path)
 
 sim_trace = Tracer.get_timed_trace(model_path, trace_path)
-# print(sim_trace)
+print(sim_trace)
 
 
 # Verifyta().set_verifyta_path(VERIFYTA_PATH)
