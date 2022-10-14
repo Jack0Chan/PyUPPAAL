13a9eddbfb33b542875a43f588e680178b89c1f0
Jack0Chan <48609550+Jack0Chan@users.noreply.github.com>
2022-10-14 17:28:35 +0800

    更新utap能用版本备份

--- src/pyuppaal/__init__.py
+++ src/pyuppaal/__init__.py
@@ -6,14 +6,4 @@ from .pyuppaal import *
 from .iTools import Mermaid
 from .utap import utap_parser
 
-
-import sys
-import os
-
-root_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(root_dir, 'utap'))
-
 __version__='0.1.14'
-
-
-# __all__ = [ROOT_DIR, TRACER_CUSTOM_WINDOWS,TRACER_CUSTOM_LINUX]
