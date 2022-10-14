c38388bc2dd0a90f2dcef913a4b6a5356f4ffc52
Jack0Chan <48609550+Jack0Chan@users.noreply.github.com>
2022-10-14 16:48:34 +0800

    将utap加入到包里，并且测试

---
+++ src/pyuppaal/utap/__init__.py
@@ -0,0 +1,31 @@
+import platform
+
+python_version = platform.python_version()
+
+utap_parser = None
+if '3.6' in python_version:
+    from .utap_36 import parse
+elif '3.7' in python_version:
+    from .utap_37 import parse
+elif '3.8' in python_version:
+    from .utap_38 import parse
+elif '3.9' in python_version:
+    from .utap_39 import parse
+elif '3.10' in python_version:
+    from .utap_310 import parse
+else:
+    raise ValueError(f'Only support python 3.6-3.10, current version: {python_version}.')
+
+utap_parser = parse
+
+def __utap_parser_api(if_str: str, xtr_file: str) -> str:
+    """接口来展示utap_parser的用法
+
+    Args:
+        if_str (str): uppaal编译出来的if文件的内容
+        xtr_file (str): `.xtr`路径文件
+
+    Returns:
+        str: 返回解析好的raw trace
+    """
+    raise NotImplemented
