02d5307f84c9f9650a663ae0dd8b4ff842c5a524
yanglinshu <nicolasvandarkholme@outlook.com>
2022-11-11 19:58:16 +0800

    Add a new test to test_tracer

--- src/tests/test_tracer.py
+++ src/tests/test_tracer.py
@@ -4,18 +4,23 @@ import os
 from pyuppaal import Tracer, UModel, Verifyta
 import verifyta_path
 
-
-def test_tracer_basic():
+def _run_test_tracer(model_path, trace_path):
     """_summary_
     """
-    Verifyta().set_verifyta_path(verifyta_path.VERIFYTA_PATH)
-    model_path = verifyta_path.bring_to_root('pedestrian.xml')
-    trace_path = verifyta_path.bring_to_root('pedestrian-1.xtr')
+    model_path = verifyta_path.bring_to_root(model_path)
+    trace_path = verifyta_path.bring_to_root(trace_path)
 
     Verifyta().easy_verify(model_path)
     sim_trace = Tracer.get_timed_trace(model_path, trace_path)
     assert os.path.exists(trace_path) is True
     os.remove(trace_path)
+    return sim_trace
+
+def test_tracer_basic():
+    """_summary_
+    """
+    Verifyta().set_verifyta_path(verifyta_path.VERIFYTA_PATH)
+    sim_trace = _run_test_tracer("pedestrian.xml", "pedestrian-1.xtr")
     # ==== save raw SimTrace ====
     raw_sim_trace_path = verifyta_path.bring_to_root('pedestrian-raw.txt')
     sim_trace.save_raw(raw_sim_trace_path)
@@ -27,3 +32,6 @@ def test_tracer_basic():
     sim_trace.save(sim_trace_path)
     assert os.path.exists(sim_trace_path) is True
     os.remove(sim_trace_path)
+
+def test_tracer_trim():
+    _run_test_tracer("test1.xml", "test1-1.xtr")
