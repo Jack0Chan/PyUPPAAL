cb47ea3740881a0051b457c04011ea11db0a9b8b
yanglinshu <nicolasvandarkholme@outlook.com>
2022-11-11 17:31:44 +0800

    Fix doc format

--- src/pyuppaal/datastruct.py
+++ src/pyuppaal/datastruct.py
@@ -4,16 +4,19 @@ from typing import List
 
 
 class TimedActions:
-    """
-    Need to
+    """ A serie of actions with lower bounds and upper bounds.
     """
     def __init__(self, actions: List[str], lb: List[str] = None, ub: List[str] = None):
         """_summary_
 
         Args:
-            actions (List[str]): ['sigOut!', 'sigOut!']
-            lb (List[str], optional): ['gclk>=20', 'x>=30']. Defaults to None.
-            ub (List[str], optional): ['gclk<=20', 'x<=30']. Defaults to None.
+            actions (List[str]): a list of actions.
+            lb (List[str], optional): a list of lowerbounds. Defaults to None.
+            ub (List[str], optional): a list of upperbounds. Defaults to None.
+
+        Examples:
+            >>> timed_actions = TimedActions(['sigOut!', 'sigOut!'], 
+            >>>     ['gclk>=20', 'x>=30'], ['gclk<=20', 'x<=30'])
         """
         self.__actions: List[str] = actions
         self.lb = lb
@@ -25,13 +28,38 @@ class TimedActions:
 
     @property
     def actions(self) -> List[str]:
+        """A serie of actions 
+
+        Examples:
+            >>> timed_actions = TimedActions(['sigOut!', 'sigOut!'], 
+            >>>     ['gclk>=20', 'x>=30'], ['gclk<=20', 'x<=30'])
+            >>> timed_actions.actions
+            ['sigOut!', 'sigOut!']
+        """
         return self.__actions
 
     @property
     def is_patterns(self) -> bool:
+        """Whether the timed actions are patterns.
+
+        Examples:
+            >>> timed_actions = TimedActions(['sigOut!', 'sigOut!'], 
+            >>>     ['gclk>=20', 'x>=30'], ['gclk<=20', 'x<=30'])
+            >>> timed_actions.is_patterns
+            False
+        """
         return all(self.lb) == -1 and all(self.ub) == -1
 
     def convert_to_list_tuple(self, clk_name='monitor_clk'):
+        """Convert to a list of tuple, each tuple is a timed action.
+        
+        Examples:
+            >>> timed_actions = TimedActions(['sigOut!', 'sigOut!'], 
+            >>>     ['gclk>=20', 'x>=30'], ['gclk<=20', 'x<=30'])
+            >>> timed_actions.convert_to_list_tuple()
+            [('sigOut!', 'gclk>=20', 'gclk<=20'), ('sigOut!', 'x>=30', 'x<=30')]
+        """
+
         self.clk_name = clk_name
         res = []
         for i in range(len(self.actions)):
@@ -39,8 +67,16 @@ class TimedActions:
         return res
 
     def convert_to_patterns(self):
+        """Convert to patterns.
+        
+        Examples:
+            >>> timed_actions = TimedActions(['sigOut!', 'sigOut!'],
+            >>>     ['gclk>=20', 'x>=30'], ['gclk<=20', 'x<=30'])
+            >>> timed_actions.convert_to_patterns()
+            ['sigOut!', 'sigOut!']
+        """
         return self.actions
 
     def __len__(self):
-        return len(self.lb)
+        return len(self.actions)
     
