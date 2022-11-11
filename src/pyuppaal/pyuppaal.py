d2a982cc79363088b77b20be8b51d5c228c71e3e
Jack0Chan <48609550+Jack0Chan@users.noreply.github.com>
2022-11-11 17:33:01 +0800

    修改文档和dmeo

--- src/pyuppaal/pyuppaal.py
+++ src/pyuppaal/pyuppaal.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 from typing import List
 from .verifyta import Verifyta
 from .umodel import UModel
+from .iTools import Mermaid
 
 
 def set_verifyta_path(verifyta_path: str):
@@ -14,7 +15,7 @@ def set_verifyta_path(verifyta_path: str):
 
     Args:
         verifyta_path (str): absolute path to `verifyta`
-    """    
+    """
     Verifyta().set_verifyta_path(verifyta_path)
 
 
@@ -34,7 +35,7 @@ def set_verifyta_path(verifyta_path: str):
 
 def easy_verify(model_path: str | List[str],
                 trace_path: str | List[str],
-                verify_options: str="-t 1",
+                verify_options: str = "-t 1",
                 num_threads=1) -> List[str]:
     """Easy verification with default options, return to the shortest diagnostic path.
     Verify the model in model_path and save the verification results to trace_path.
@@ -56,44 +57,58 @@ def easy_verify(model_path: str | List[str],
 def verify(model_path: str | List[str],
            verify_options: str | List[str] = None,
            num_threads: int = 1) -> List[str]:
-        """Verify models and return the verify results as list.
+    """Verify models and return the verify results as list.
 
-        This is designed for advanced UPPAAL user.
-        If you want to save a `.xtr` or `.xml`(DBM) path, you may want to check `Verifyta().easy_verify()`.
-        WARNING: Note that `-f xx.xtr` or `-X xx.xml` should be used together with `-t` options, otherwise you may fail to save the path files.
-        
-        Examples:
-        ```python
-        Verifyta().verify('test1.xml')
-        Verifyta().verify(['test1.xml', 'test2.xml'], verify_options = '-t 1 -o 0')
-        Verifyta().verify(['test1.xml', 'test1.xml'], verify_options = ['-t 1 -o 0', '-t 2 -o 0'])
-        # if you surely want to generate a trace file with Verifyta().verify()
-        # you should not add `.xtr` at the end of `xtr_trace`, or `.xml` at the end of `xtr_trace`
-        Verifyta().verify(['test1.xml', 'test1.xml'], verify_options = ['-f xtr_trace -t 1 -o 0', '-X xml_trace -t 2 -o 0'], num_threads=2)
-        ```
+    This is designed for advanced UPPAAL user.
+    If you want to save a `.xtr` or `.xml`(DBM) path, you may want to check `Verifyta().easy_verify()`.
+    WARNING: Note that `-f xx.xtr` or `-X xx.xml` should be used together with `-t` options, otherwise you may fail to save the path files.
 
-        Args:
-            model_path (str | List[str]): model paths to be verified.
-            verify_options (str | List[str], optional): verify options that are proveded by `verifyta`, and you can get details by run `verifyta -h` in your terminal.
-                If `verify_options` is provided as a single `string`, all the models will be verified with the same options. Defaults to None.
-            num_threads (int, optional): use multi-threads if is greater than 1. Defaults to 1.
+    Examples:
+    ```python
+    Verifyta().verify('test1.xml')
+    Verifyta().verify(['test1.xml', 'test2.xml'], verify_options = '-t 1 -o 0')
+    Verifyta().verify(['test1.xml', 'test1.xml'], verify_options = ['-t 1 -o 0', '-t 2 -o 0'])
+    # if you surely want to generate a trace file with Verifyta().verify()
+    # you should not add `.xtr` at the end of `xtr_trace`, or `.xml` at the end of `xtr_trace`
+    Verifyta().verify(['test1.xml', 'test1.xml'], verify_options = ['-f xtr_trace -t 1 -o 0', '-X xml_trace -t 2 -o 0'], num_threads=2)
+    ```
 
-        Raises:
-            ValueError: _description_
-            TypeError: _description_
-            ValueError: _description_
-            FileNotFoundError: _description_
-            ValueError: _description_
+    Args:
+        model_path (str | List[str]): model paths to be verified.
+        verify_options (str | List[str], optional): verify options that are proveded by `verifyta`, and you can get details by run `verifyta -h` in your terminal.
+            If `verify_options` is provided as a single `string`, all the models will be verified with the same options. Defaults to None.
+        num_threads (int, optional): use multi-threads if is greater than 1. Defaults to 1.
 
-        Returns:
-            List[str]: terminal verify results for each `.xml` model. 
-            Example:
-            ['Options for the verification:\n  Generating shortest trace\n  Search order is breadth first\n  Using conservative space optimisation\n  Seed is 1665658616\n  State space representation uses minimal constraint systems\n\x1b[2K\nVerifying formula 1 at /nta/queries/query[1]/formula
-             \n\x1b[2K -- Formula is satisfied.\n']
-            ['Options for the verification:\n  Generating shortest trace\n  Search order is breadth first\n  Using conservative space optimisation\n  Seed is 1665658616\n  State space representation uses minimal constraint systems\n\x1b[2K\nVerifying formula 1 at /nta/queries/query[1]/formula
-             \n\x1b[2K -- Formula is NOT satisfied.\n']
-        """
-        return Verifyta().verify(model_path, verify_options, num_threads)
+    Raises:
+        ValueError: _description_
+        TypeError: _description_
+        ValueError: _description_
+        FileNotFoundError: _description_
+        ValueError: _description_
+
+    Returns:
+        List[str]: terminal verify results for each `.xml` model. 
+        Example:
+        ['Options for the verification:\n  Generating shortest trace\n  Search order is breadth first\n  Using conservative space optimisation\n  Seed is 1665658616\n  State space representation uses minimal constraint systems\n\x1b[2K\nVerifying formula 1 at /nta/queries/query[1]/formula
+         \n\x1b[2K -- Formula is satisfied.\n']
+        ['Options for the verification:\n  Generating shortest trace\n  Search order is breadth first\n  Using conservative space optimisation\n  Seed is 1665658616\n  State space representation uses minimal constraint systems\n\x1b[2K\nVerifying formula 1 at /nta/queries/query[1]/formula
+         \n\x1b[2K -- Formula is NOT satisfied.\n']
+    """
+    return Verifyta().verify(model_path, verify_options, num_threads)
+
+
+def get_communication_graph(model_path: str, save_path=None, is_beautify=True) -> Mermaid:
+    """Get the communication graph of the UPPAAL model, and return a `Mermaid` instance.
+
+    Args:
+        save_path (_type_, optional): `<.md | .svg | .pdf | .png>`, the path to save the file. Defaults to None.
+        is_beautify (bool, optional): whether beautify the mermaid file by merging edges. Defaults to True.
+
+    Returns:
+        Mermaid: _description_
+    """
+    umod = UModel(model_path=model_path)
+    return umod.get_communication_graph(save_path, is_beautify)
 
 
 def cmd(cmd: str):
@@ -119,4 +134,3 @@ def cmds(self, cmds: List[str], num_threads: int = 1) -> List[str]:
         List[str]: return values of each command.
     """
     return Verifyta().cmds(cmds, num_threads)
-
